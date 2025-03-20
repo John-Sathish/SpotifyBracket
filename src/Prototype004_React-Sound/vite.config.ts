acfc5a9bf285dd7a9dcfca2d72758b57a3d5b6b1
john sathish <johnsathish2004@gmail.com>
2025-03-20 01:09:13 +0000

    deploy

--- src/Prototype004_React-Sound/vite.config.ts
+++ src/Prototype004_React-Sound/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Prototype004_React-Sound/',
+  base: '/SpotifyBracket/',
   plugins: [react()],
   resolve: {
     dedupe: ['@types/react-router-dom','react','react-dom','react-router-dom','react-use-cookie'],
